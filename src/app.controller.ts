8777da5ea85038949a5aac7075f151e02779d4f6
Cezary Grzanka <cezarygrzanka@gmail.com>
2020-03-27 01:58:41 +0100

    Add all specializations

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -4,6 +4,59 @@ import { CreateUserDto } from './dto/create-user.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository, getConnection } from 'typeorm';
 
+const SPEC: string[] = [
+  'Agronomia',
+  'Analityka chemiczna i spożywcza',
+  'Architektura',
+  'Architektura i urbanistyka',
+  'Architektura krajobrazu',
+  'Architektura wnętrz',
+  'Bioinżynieria w produkji zwierzęcej',
+  'Biotechnologia',
+  'Budowa i eksploatacja maszyn',
+  'Budownictwo',
+  'Computer aided engineering',
+  'Elektronika i telekomunikacja',
+  'Elektrotechnika',
+  'Energetyka',
+  'Finanse i rachunkowość',
+  'Fizyka techniczna',
+  'Geodezja i kartografia',
+  'Gospodarowanie zasobami naturalnymi',
+  'Informatyka Stosowana',
+  'Inspekcja weterynaryjna',
+  'Inżynieria biomedyczna',
+  'Inżynieria gazu łupkowego',
+  'Inżynieria materiałowa',
+  'Inżynieria odnawialnych źródeł energii',
+  'Inżynieria środowiskowa',
+  'Inżynieria w biologii stosowanej',
+  'Mechaniczna inżynieria tworzyw',
+  'Mechanika i budowa maszyn',
+  'Mechatronika',
+  'Nanobioinżynieria',
+  'Ochrona i kształtowanie środowiska',
+  'Ochrona środowiska',
+  'Ogrody zoologiczne i zwierzęta amatorskie',
+  'Przetwórstwo tworzyw sztucznych',
+  'Rolnictwo',
+  'Rolnictwo ANG',
+  'Rolnictwo w języku angielskim',
+  'Szkoła doktorska',
+  'Technika rolnicza i leśna',
+  'Technologia chemiczna',
+  'Technologia żywności i żywienie człowieka',
+  'Teleinformatyka',
+  'Transport',
+  'Weterynaria',
+  'Wzornictwo',
+  'Zarządzanie',
+  'Zarządzanie i inżynieria produkcji',
+  'Zielarstwo i fitoteriapia',
+  'Zoofizjoterapia',
+  'Zootechnika',
+];
+
 @Controller()
 export class AppController {
   constructor(
@@ -26,10 +79,6 @@ export class AppController {
 
   @Post('/register')
   async show(@Body() createUserDto: CreateUserDto) {
-    console.log(createUserDto);
-
-    const SPEC = ['ist', 'tele'];
-
     await getConnection()
       .createQueryBuilder()
       .insert()
