1f7e8b175db43948cbb60615698492e2bd5fead5
Cezary Grzanka <cezarygrzanka@gmail.com>
2020-03-27 02:32:31 +0100

    Add adding student logic -> prevent duplicates '

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,68 +1,11 @@
-import { User } from './app.entitie';
+import { AppService } from './app.service';
 import { Controller, Get, Render, Post, Body } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
-import { InjectRepository } from '@nestjs/typeorm';
-import { Repository, getConnection } from 'typeorm';
-
-const SPEC: string[] = [
-  'Agronomia',
-  'Analityka chemiczna i spożywcza',
-  'Architektura',
-  'Architektura i urbanistyka',
-  'Architektura krajobrazu',
-  'Architektura wnętrz',
-  'Bioinżynieria w produkji zwierzęcej',
-  'Biotechnologia',
-  'Budowa i eksploatacja maszyn',
-  'Budownictwo',
-  'Computer aided engineering',
-  'Elektronika i telekomunikacja',
-  'Elektrotechnika',
-  'Energetyka',
-  'Finanse i rachunkowość',
-  'Fizyka techniczna',
-  'Geodezja i kartografia',
-  'Gospodarowanie zasobami naturalnymi',
-  'Informatyka Stosowana',
-  'Inspekcja weterynaryjna',
-  'Inżynieria biomedyczna',
-  'Inżynieria gazu łupkowego',
-  'Inżynieria materiałowa',
-  'Inżynieria odnawialnych źródeł energii',
-  'Inżynieria środowiskowa',
-  'Inżynieria w biologii stosowanej',
-  'Mechaniczna inżynieria tworzyw',
-  'Mechanika i budowa maszyn',
-  'Mechatronika',
-  'Nanobioinżynieria',
-  'Ochrona i kształtowanie środowiska',
-  'Ochrona środowiska',
-  'Ogrody zoologiczne i zwierzęta amatorskie',
-  'Przetwórstwo tworzyw sztucznych',
-  'Rolnictwo',
-  'Rolnictwo ANG',
-  'Rolnictwo w języku angielskim',
-  'Szkoła doktorska',
-  'Technika rolnicza i leśna',
-  'Technologia chemiczna',
-  'Technologia żywności i żywienie człowieka',
-  'Teleinformatyka',
-  'Transport',
-  'Weterynaria',
-  'Wzornictwo',
-  'Zarządzanie',
-  'Zarządzanie i inżynieria produkcji',
-  'Zielarstwo i fitoteriapia',
-  'Zoofizjoterapia',
-  'Zootechnika',
-];
+import { User } from './app.entitie';
 
 @Controller()
 export class AppController {
-  constructor(
-    @InjectRepository(User)
-    private usersRepository: Repository<User>,
-  ) {}
+  constructor(private readonly appService: AppService) {}
 
   @Get()
   @Render('pages/home')
@@ -78,27 +21,12 @@ export class AppController {
   }
 
   @Post('/register')
-  async show(@Body() createUserDto: CreateUserDto) {
-    await getConnection()
-      .createQueryBuilder()
-      .insert()
-      .into(User)
-      .values([
-        {
-          indexNumber: createUserDto.indexNumber,
-          firstName: createUserDto.firstname,
-          lastName: createUserDto.lastname,
-          email: createUserDto.email,
-          specialization: SPEC[createUserDto.specialization],
-        },
-      ])
-      .execute();
-
-    return createUserDto;
+  async show(@Body() createUserDto: CreateUserDto): Promise<object> {
+    return await this.appService.createUser(createUserDto);
   }
 
   @Get('/all')
-  getAll(): object {
-    return this.usersRepository.find();
+  async getAll(): Promise<User[]> {
+    return await this.appService.getUsers();
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,7 +1,61 @@
+import { CreateUserDto } from './dto/create-user.dto';
 import { User } from './app.entitie';
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { Repository, getConnection } from 'typeorm';
+
+const SPEC: string[] = [
+  'Agronomia',
+  'Analityka chemiczna i spożywcza',
+  'Architektura',
+  'Architektura i urbanistyka',
+  'Architektura krajobrazu',
+  'Architektura wnętrz',
+  'Bioinżynieria w produkji zwierzęcej',
+  'Biotechnologia',
+  'Budowa i eksploatacja maszyn',
+  'Budownictwo',
+  'Computer aided engineering',
+  'Elektronika i telekomunikacja',
+  'Elektrotechnika',
+  'Energetyka',
+  'Finanse i rachunkowość',
+  'Fizyka techniczna',
+  'Geodezja i kartografia',
+  'Gospodarowanie zasobami naturalnymi',
+  'Informatyka Stosowana',
+  'Inspekcja weterynaryjna',
+  'Inżynieria biomedyczna',
+  'Inżynieria gazu łupkowego',
+  'Inżynieria materiałowa',
+  'Inżynieria odnawialnych źródeł energii',
+  'Inżynieria środowiskowa',
+  'Inżynieria w biologii stosowanej',
+  'Mechaniczna inżynieria tworzyw',
+  'Mechanika i budowa maszyn',
+  'Mechatronika',
+  'Nanobioinżynieria',
+  'Ochrona i kształtowanie środowiska',
+  'Ochrona środowiska',
+  'Ogrody zoologiczne i zwierzęta amatorskie',
+  'Przetwórstwo tworzyw sztucznych',
+  'Rolnictwo',
+  'Rolnictwo ANG',
+  'Rolnictwo w języku angielskim',
+  'Szkoła doktorska',
+  'Technika rolnicza i leśna',
+  'Technologia chemiczna',
+  'Technologia żywności i żywienie człowieka',
+  'Teleinformatyka',
+  'Transport',
+  'Weterynaria',
+  'Wzornictwo',
+  'Zarządzanie',
+  'Zarządzanie i inżynieria produkcji',
+  'Zielarstwo i fitoteriapia',
+  'Zoofizjoterapia',
+  'Zootechnika',
+];
 
 @Injectable()
 export class AppService {
@@ -10,7 +64,48 @@ export class AppService {
     private usersRepository: Repository<User>,
   ) {}
 
-  getUsers(): object {
-    return this.usersRepository.find();
+  async getUsers(): Promise<User[]> {
+    return await this.usersRepository.find();
+  }
+
+  async createUser(user: CreateUserDto): Promise<object> {
+    const users: User[] = await this.getUsers();
+
+    const conflicts: User = users.find(el => {
+      return el.email === user.email;
+    });
+
+    if (conflicts) {
+      return {
+        status: {
+          name: 'error',
+          message: 'User already exists',
+        },
+        user,
+      };
+    } else {
+      await getConnection()
+        .createQueryBuilder()
+        .insert()
+        .into(User)
+        .values([
+          {
+            indexNumber: user.indexNumber,
+            firstName: user.firstname,
+            lastName: user.lastname,
+            email: user.email,
+            specialization: SPEC[user.specialization],
+          },
+        ])
+        .execute();
+
+      return {
+        status: {
+          name: 'ok',
+          message: 'created',
+        },
+        user,
+      };
+    }
   }
 }
